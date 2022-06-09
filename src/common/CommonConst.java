6ed18e4cd3300b032f664ca905cab5188ce98fb1
YANHAOsTHINGSdb <47000444+YANHAOsTHINGSdb@users.noreply.github.com>
2022-06-09 15:04:58 +0900

    update

--- src/common/CommonConst.java
+++ src/common/CommonConst.java
@@ -15,4 +15,10 @@ public class CommonConst {
 	public final static int 如果_手持高点_要找底点_但没碰见一个更高的谷=4;
 	public final static int 如果_這是第一次計算=5;
 	public final static int 如果_這是最後一次計算=6;
+	
+	public final static int 日時 = 0;
+	public final static int 開盤價 = 1;
+	public final static int 收盤價 = 2;
+	public final static int 最低價 = 3;
+	public final static int 最高價 = 4;
 }
