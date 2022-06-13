e974aafee22689c266971e767f13a3b3b283fda5
YANHAOsTHINGSdb <47000444+YANHAOsTHINGSdb@users.noreply.github.com>
2022-06-13 23:10:56 +0900

    update

--- src/InputData/計算目標.java
+++ src/InputData/計算目標.java
@@ -6,6 +6,15 @@ import lombok.Data;
 @Data
 public class 計算目標 {
 
+	public 計算目標(計算目標 o計算目標) {
+		求高低 =o計算目標.求高低;
+		确 = o計算目標.get确();
+		假 = o計算目標.get假();
+		求 = o計算目標.get求();
+	}
+	public 計算目標() {
+		
+	}
 	int 求高低;
 	折点  确;
 	折点  假;
